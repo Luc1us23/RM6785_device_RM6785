e87fe2745b980445f6562d03dde1e4dbf05acd93
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2025-06-10 14:32:27 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
+git clone https://github.com/albatron34000/RM6785_device_RM6785 device/realme/RM6785
+git clone https://github.com/albatron34000/RM6785_device-mediatek-sepolicy_vndr device/mediatek/sepolicy_vndr 
+git clone https://github.com/albatron34000/RM6785_vendor_RM6785-common vendor/RM6785-common
+git clone https://github.com/albatron34000/RM6785_hardware_oplus hardware/oplus
+git clone https://github.com/albatron34000/RM6785_hardware_mediatek hardware/mediatek
+git clone https://github.com/albatron34000/RM6785_hardware_dolby hardware/dolby
+git clone https://github.com/albatron34000/RM6785_kernel_realme_mt6785 kernel/realme/mt6785
 # Clang:
 git clone --depth=1 https://gitlab.com/LeCmnGend/clang -b clang-19 toolchain/clang-proton
 
